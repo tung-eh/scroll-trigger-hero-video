9bcb2df459e94379d82c7bf8a466b0568b4782af
Tung Teng <thetungctn@gmail.com>
2026-01-05 16:46:01 +0700

    Navar style

--- src/App/Navbar.tsx
+++ src/App/Navbar.tsx
@@ -1,3 +1,5 @@
+import Button from 'src/components/Button'
+
 const navLinks = [
   { href: '#', name: 'Overview' },
   { href: '#', name: 'Solutions' },
@@ -6,24 +8,32 @@ const navLinks = [
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className="fixed w-full grid grid-cols-3 items-center gap-8 py-6 px-8">
+      <div className="flex gap-12">
         {navLinks.map(({ href, name }) => (
-          <a href={href}>{name}</a>
+          <a
+            href={href}
+            className="uppercase text-[0.8rem] font-medium text-foreground"
+          >
+            {name}
+          </a>
         ))}
       </div>
       <div>
-        <a href="#">
-          <img src="/logo.png" /> Byewind
+        <a
+          href="#"
+          className="font-hg text-[1.5rem] font-medium text-foreground flex items-center justify-center gap-2"
+        >
+          <img src="/logo.png" className="w-6" /> Byewind
         </a>
       </div>
-      <div>
-        <div>
-          <a href="#">Live Demo</a>
-        </div>
-        <div>
-          <a href="#">Get started</a>
-        </div>
+      <div className="flex justify-end gap-6">
+        <a href="#">
+          <Button>Live Demo</Button>
+        </a>
+        <a href="#">
+          <Button variant="secondary">Get started</Button>
+        </a>
       </div>
     </nav>
   )
