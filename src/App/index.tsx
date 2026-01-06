00e4cdc1780784c5356183f0c2fa25bc32a48c8c
Tung Teng <thetungctn@gmail.com>
2026-01-06 12:14:04 +0700

    Setup gsap scroll trigger & lenis

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,8 +1,24 @@
+import { useEffect } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+import Lenis from 'lenis'
+
 import Navbar from './Navbar'
 import Hero from './Hero'
 import Outro from './Outro'
 
+gsap.registerPlugin(ScrollTrigger)
+
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis()
+    lenis.on('scroll', ScrollTrigger.update)
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000)
+    })
+    gsap.ticker.lagSmoothing(0)
+  }, [])
+
   return (
     <main>
       <Navbar />
